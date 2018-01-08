92f9ed871dc5c55982592cc7408220e38f622754
JackyYuen <562422313@qq.com>
2018-01-08 09:27:25 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,6 @@
+
+#include<stdio.h>
+main(__)size_t __;??<printf("",("Zitian")[0]-64-25,(__==0?("Zitian"<"Dacai"?
+putchar(1["Dacai":>):putchar(__^(__-1)["Dacai"??))):"Hello">"World"?putchar(*
+(main(0),"Hello")):(__&2&&__>11)?__:__&2?(putchar(*("el Wld"+(__-1>>1))),
+(main(++__))):(putchar(*("Hloor\n"+(__-1>>1))),(main(++__)))));%>
